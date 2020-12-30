fedf1f7ac337cd0c3779de77f2e4ec6045c3d45c
Nikolas Garcia <nikolasgarcia@Nicks-MacBook-Air.local>
2020-12-30 15:58:50 -0800

    Add add-person form

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def createGroupPage():
     return render_template("index.html")
+
+@app.route("/add-person",methods=["POST"])
+def addPerson():
+    return redirect("/")
